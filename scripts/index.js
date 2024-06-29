7cc7dbd18f53913a8c6c6ade8c5416fe81fa9da2
Anastasia <leatrix5@yandex.ru>
2024-06-29 19:26:05 +0400

    create index.js and add photo

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+// Объявление констант
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+// Функция создание карточек
 
-// @todo: Функция создания карточки
+const cardCreate = (cardName, cardLink, cardDelete) => {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
 
-// @todo: Функция удаления карточки
+    cardElement.querySelector('.card__title').textContent = cardName;
+    cardImage.alt = cardName;
+    cardImage.src = cardLink;
 
-// @todo: Вывести карточки на страницу
+    const cardButtonDelete = cardElement.querySelector('.card__delete-button');
+        cardButtonDelete.addEventListener('click', function(evt) {
+            cardDelete(evt);
+        });
+
+    return cardElement;
+};
+
+// Функция удаления карточки
+
+const cardDelete = (evt) => {
+    const element = evt.target.closest('.card');
+    element.remove();
+};
+
+// Вывод карточек на страничку
+
+initialCards.forEach(({name, link}) => {
+    const cardData = cardCreate(name, link, cardDelete);
+    cardList.append(cardData);
+});
