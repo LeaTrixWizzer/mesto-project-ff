4da1d448c65d794eed13e7d9f1d63f7c39fb3d9d
Anastasia <leatrix5@yandex.ru>
2024-07-27 20:50:32 +0400

    fixed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,78 +1,107 @@
 // Импорт данных
 
-import '../index.css';
-import { initialCards } from './cards';
-import { openModal, closeModal } from './modal';
-import { createCard, deleteCard, likeCard, clickHandleImage } from './card';
+import "../index.css";
+import { initialCards } from "./cards";
+import { openModal, closeModal } from "./modal";
+import { createCard, deleteCard, likeCard } from "./card";
 
 // Объявление констант
 
-const cardList = document.querySelector('.places__list');
+const cardList = document.querySelector(".places__list");
 
-const buttonEditProfile = document.querySelector('.profile__edit-button');
-const profileName = document.querySelector('.profile__title');
-const profileJob = document.querySelector('.profile__description');
+const buttonEditProfile = document.querySelector(".profile__edit-button");
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__description");
 
-const buttonAddCard = document.querySelector('.profile__add-button');
+const buttonAddCard = document.querySelector(".profile__add-button");
 
-const modalEditProfile = document.querySelector('.popup_type_edit');
-const editProfileForm = modalEditProfile.querySelector('.popup__form');
+const modalEditProfile = document.querySelector(".popup_type_edit");
+const editProfileForm = modalEditProfile.querySelector(".popup__form");
 const nameInput = editProfileForm.querySelector('input[name="name"]');
 const jobInput = editProfileForm.querySelector('input[name="description"]');
 
-const modalAddCard = document.querySelector('.popup_type_new-card');
-const addForm = modalAddCard.querySelector('.popup__form');
+const modalAddCard = document.querySelector(".popup_type_new-card");
+const addForm = modalAddCard.querySelector(".popup__form");
 const cardNameInput = addForm.querySelector('input[name="place-name"]');
 const cardLinkInput = addForm.querySelector('input[name="link"]');
 
+// Функция обработки клика на изображение карточки
+
+export const clickHandleImage = (evt) => {
+  const element = evt.target.closest(".card");
+  const cardImage = element.querySelector(".card__image");
+  const cardTitle = element.querySelector(".card__title");
+
+  const previewImage = document.querySelector(".popup__image");
+  const captionModal = document.querySelector(".popup__caption");
+  const previewImageModal = document.querySelector(".popup_type_image");
+
+  openModal(previewImageModal);
+
+  previewImage.src = cardImage.src;
+  previewImage.alt = cardTitle.name;
+
+  captionModal.textContent = cardTitle.textContent;
+};
+
 // Вывод карточек на страничку
 
-initialCards.forEach(({name, link}) => {
-    const cardData = createCard(name, link, likeCard, deleteCard, clickHandleImage);
-    cardList.append(cardData);
+initialCards.forEach(({ name, link }) => {
+  const cardData = createCard(
+    name,
+    link,
+    likeCard,
+    deleteCard,
+    clickHandleImage
+  );
+  cardList.append(cardData);
 });
 
 // Функция открытия модального окна при нажатии
 
 const openModalEditProfile = () => {
-    openModal(modalEditProfile);
+  openModal(modalEditProfile);
 
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 };
 
 const openModalAddCard = () => {
-    addForm.reset();
-    openModal(modalAddCard);
+  openModal(modalAddCard);
 };
 
 // Функция при нажатии на клавишу "Сохранить"
 
 const submitEditProfileForm = (evt) => {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
 
-    editProfileForm.reset();
+  editProfileForm.reset();
 
-    closeModal(modalEditProfile);
+  closeModal(modalEditProfile);
 };
 
 const submitAddForm = (evt) => {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    const addCard = createCard(cardNameInput.value, cardLinkInput.value, likeCard, deleteCard)
-    cardList.prepend(addCard);
+  const addCard = createCard(
+    cardNameInput.value,
+    cardLinkInput.value,
+    likeCard,
+    deleteCard
+  );
+  cardList.prepend(addCard);
 
-    addForm.reset();
-    closeModal(modalAddCard);
-}
+  addForm.reset();
+  closeModal(modalAddCard);
+};
 
 // Обработчики событий
 
-buttonEditProfile.addEventListener('click', openModalEditProfile);
-editProfileForm.addEventListener('submit', submitEditProfileForm);
+buttonEditProfile.addEventListener("click", openModalEditProfile);
+editProfileForm.addEventListener("submit", submitEditProfileForm);
 
-buttonAddCard.addEventListener('click', openModalAddCard);
-addForm.addEventListener('submit', submitAddForm);
+buttonAddCard.addEventListener("click", openModalAddCard);
+addForm.addEventListener("submit", submitAddForm);
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,58 +1,57 @@
 // Функция открытия модального окна
 
 const openModal = (modal) => {
-    modal.classList.add("popup_is-opened");
-    attachModalEvents();
+  modal.classList.add("popup_is-opened");
+  attachModalEvents();
 };
 
 // Функция закрытия модального окна
 
 const closeModal = (modal) => {
-    modal.classList.remove("popup_is-opened");
-    detahModalEvents();
+  modal.classList.remove("popup_is-opened");
+  removeModalListeners();
 };
 
 //Функция обработки нажатия клавиши Esc
 
 const handleEscapes = (evt) => {
-    if (evt.key === "Escape") {
-        const currentPopup = document.querySelector(".popup_is-opened");
-        closeModal(currentPopup);
-    }
+  if (evt.key === "Escape") {
+    const currentPopup = document.querySelector(".popup_is-opened");
+    closeModal(currentPopup);
+  }
 };
 
 // Функция обработки клика вне модального окна
 
 const handleOutside = (evt) => {
-    if(evt.target.classList.contains('popup')) {
-        closeModal(evt.target);
-    }
+  if (evt.target.classList.contains("popup")) {
+    closeModal(evt.target);
+  }
 };
 
 // Функция обработки клика на крестик
 
 const handleCross = (evt) => {
-    if (evt.target.closest('.popup__close')) {
-        closeModal(evt.target.closest('.popup'));
+  if (evt.target.closest(".popup__close")) {
+    closeModal(evt.target.closest(".popup"));
   }
 };
-
 
 // Обработчик событии к элементам модального окна при открытии
 
 const attachModalEvents = () => {
-    document.addEventListener('click', handleCross);
-    document.addEventListener("keydown", handleEscapes);
-    document.addEventListener("click", handleOutside);
-}
+  document.addEventListener("click", handleCross);
+  document.addEventListener("keydown", handleEscapes);
+  document.addEventListener("click", handleOutside);
+};
 
 // Обработчик событии к элементам модального окна при закрытии
 
-const detahModalEvents = () => {
-    document.removeEventListener('click', handleCross);
-    document.removeEventListener("keydown", handleEscapes);
-    document.removeEventListener("click", handleOutside);
-}
+const removeModalListeners = () => {
+  document.removeEventListener("click", handleCross);
+  document.removeEventListener("keydown", handleEscapes);
+  document.removeEventListener("click", handleOutside);
+};
 
 // Экспорт данных
 
